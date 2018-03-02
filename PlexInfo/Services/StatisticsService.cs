4d682ab202aa8bd126c39e588cbf4b8b5ee32d24
hrannar <hrannar@motus.is>
2018-03-02 14:53:12 +0000

    added statistics service

---
+++ PlexInfo/Services/StatisticsService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PlexInfo.Models;
+
+namespace PlexInfo.Services
+{
+    public interface IStatisticsService
+    {
+        
+    }
+
+    class StatisticsService : IStatisticsService
+    {
+        private readonly ILogger<StatisticsService> _logger;
+        private readonly AppSettings _config;
+
+        public StatisticsService(
+            ILogger<StatisticsService> logger,
+            IOptions<AppSettings> config)
+        {
+            _logger = logger;
+            _config = config.Value;
+        }
+    }
+}
